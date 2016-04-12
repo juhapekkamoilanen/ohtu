912661f3c51dcb55118b66eb5473406d14e326de
Anni <nadeka@users.noreply.github.com>
2016-04-12 16:16:35 +0300

    Create project skeleton

---
+++ src/main/java/com/ihasama/ohtu/App.java
@@ -0,0 +1,13 @@
+package com.ihasama.ohtu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
