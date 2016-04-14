be96a2aa334889b5c8234f9c42ca96b3d41f236e
Olavi Mustanoja <tilastokeskus@gmail.com>
2016-04-14 06:57:08 +0300

    References can be added (to memory)

--- src/main/java/com/ihasama/ohtu/App.java
+++ src/main/java/com/ihasama/ohtu/App.java
@@ -1,23 +1,91 @@
 package com.ihasama.ohtu;
 
+import com.ihasama.ohtu.domain.EntryType;
+import com.ihasama.ohtu.domain.FieldType;
+import com.ihasama.ohtu.domain.Reference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
-public class App 
-{
-    public static void main( String[] args )
-    {
-
+public class App {
+    public static void main( String[] args ) {
         ApplicationContext ctx = new FileSystemXmlApplicationContext("src/main/resources/spring-context.xml");
 
         App application = ctx.getBean(App.class);
         application.run();
     }
 
-    private void run()
-    {
-        System.out.println("Hello world!");
+    private void run() {
+        List<Reference> references = new ArrayList();
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            EntryType entryType;
+            String id;
+                    
+            while (true) {
+                try {
+                    System.out.print("Reference type: ");
+                    entryType = EntryType.valueOf(sc.nextLine().toUpperCase());
+                    break;
+                } catch (IllegalArgumentException ex) {
+                    System.out.println("Illegal reference type.");
+                }
+            }
+            
+            System.out.print("Reference id: ");
+            do {
+                id = sc.nextLine();
+            } while (id.isEmpty());
+            
+            Reference ref = new Reference(entryType, id);
+            
+            FieldType fieldType;
+            String value;
+            
+            askfields: // label loop to jump out of it
+            while (true) {
+                
+                while (true) {
+                    try {
+                        System.out.print("Field type (empty to save): ");
+                        
+                        String str = sc.nextLine();
+                        if (str.isEmpty())
+                            break askfields; // jump out of outer loop
+                        
+                        fieldType = FieldType.valueOf(str.toUpperCase());
+                        break;
+                    } catch (IllegalArgumentException ex) {
+                        System.out.println("Illegal field type.");
+                    }
+                }
+                
+                System.out.print("Value: ");
+                do {
+                    value = sc.nextLine();
+                } while (value.isEmpty());
+                
+                ref.addField(fieldType, value);
+                System.out.println("Field added.\n");
+            }
+            
+            references.add(ref);
+            System.out.println("Reference added successfully.");
+            System.out.println("Add another (y/n)?");
+            String answer = sc.nextLine();
+            
+            if (answer.equals("n"))
+                break;
+        }
+        
+        for (Reference ref : references) {
+            System.out.println(ref);
+            System.out.println();
+        }
     }
 }
--- src/main/java/com/ihasama/ohtu/domain/Reference.java
+++ src/main/java/com/ihasama/ohtu/domain/Reference.java
@@ -10,11 +10,45 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Reference {
-    private final EntryType type;
+    private final EntryType entryType;
+    private final String id;
     private final Map<FieldType, String> fields;
     
-    public Reference(EntryType type) {
-        this.type = type;
+    public Reference(EntryType type, String id) {
+        this.entryType = type;
+        this.id = id;
         this.fields = new HashMap<>();
+    }
+    
+    public EntryType getType() {
+        return this.entryType;
+    }
+    
+    public String getId() {
+        return this.id;
+    }
+    
+    public boolean addField(FieldType type, String value) {
+        if (fields.containsKey(type))
+            return false;
+        
+        fields.put(type, value);
+        return true;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("@").append(entryType.toString().toLowerCase());
+        sb.append("{").append(id).append(",\n");
+        
+        for (Map.Entry<FieldType, String> e : fields.entrySet()) {
+            String type = e.getKey().toString().toLowerCase();
+            String value = e.getValue();
+            sb.append(type).append(" = {").append(value).append("},\n");
+        }
+        
+        sb.append("}");
+        return sb.toString();
     }
 }
