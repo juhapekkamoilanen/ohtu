474b807da5388e9bd3e010a4ed725573298fb664
Anni <nadeka@users.noreply.github.com>
2016-04-19 18:57:35 +0300

    Convert accented characters

--- src/main/java/com/ihasama/ohtu/domain/Reference.java
+++ src/main/java/com/ihasama/ohtu/domain/Reference.java
@@ -13,7 +13,7 @@ public class Reference {
     private EntryType entryType;
     private String id;
     private Map<FieldType, String> fields;
-    
+
     public Reference(EntryType type, String id) {
         this.entryType = type;
         this.id = id;
@@ -41,20 +41,41 @@ public class Reference {
         StringBuilder sb = new StringBuilder();
         sb.append("@").append(entryType.toString().toLowerCase());
         sb.append("{").append(id).append(",\n");
-        
+
         for (Map.Entry<FieldType, String> e : fields.entrySet()) {
             String type = e.getKey().toString().toLowerCase();
-            String value = e.getValue();
+            String value = convertAccentedChars(e.getValue());
             sb.append(type).append(" = {").append(value).append("},\n");
         }
-        
+
         sb.append("}");
         return sb.toString();
+    }
+
+    private String convertAccentedChars(String string) {
+        StringBuilder newString = new StringBuilder();
+
+        for (int i = 0; i< string.length(); i++) {
+            char c = string.charAt(i);
+
+            switch (c) {
+                case 'ä': newString.append("{\\\"a}"); break;
+                case 'ö': newString.append("{\\\"o}"); break;
+                case 'Ä': newString.append("{\\\"A}"); break;
+                case 'Ö': newString.append("{\\\"O}"); break;
+                case 'Å': newString.append("{\\\"AA}"); break;
+                case 'å': newString.append("{\\\"aa}"); break;
+                default: newString.append(c);
+            }
+        }
+
+        return newString.toString();
     }
 
     public Map<FieldType, String> getFields() {
         return fields;
     }
+
 
     public void setType(EntryType entryType) {
         this.entryType = entryType;
@@ -67,6 +88,4 @@ public class Reference {
     public void setFields(Map<FieldType, String> fields) {
         this.fields = fields;
     }
-    
-    
 }
