f690dd84b15fcd8a3da884ecee46eb8034378a41
Anni <nadeka@users.noreply.github.com>
2016-04-29 17:28:13 +0300

    Fix weird mistake in refmemdao

--- src/main/java/com/ihasama/ohtu/data_access/ReferenceMemoryDao.java
+++ src/main/java/com/ihasama/ohtu/data_access/ReferenceMemoryDao.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-public class git ReferenceMemoryDao extends MemoryDao<Reference> {
+public class ReferenceMemoryDao extends MemoryDao<Reference> {
 
     public List<Reference> getObjects(String filter) {
         if (filter == null || filter.isEmpty()) {
